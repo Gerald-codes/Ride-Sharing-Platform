1fd8e5f17bbf6547b2b5d80dc2528b52912c5b93
Gerald Tan Yuan Xin (ZNX) <gerald.tan@zionext.com>
2021-12-07 22:52:07 +0800

    Able to Insert to DB from UI (hardcodedData)

--- src/UI/main.go
+++ src/UI/main.go
@@ -1,16 +1,16 @@
-package UIMain
+package main
 
 import (
 	"fmt"
-	"/Users/gerald/Desktop/SEM 3.2/ETI/Assignment/Ride-Sharing-Platform/src/Driver_MS"
+	DriverMain "importMods/Driver_MS"
 )
 
-func UIMain() {
+func main() {
 	var MenuOption, CreateAccMenuOption float32
 	var Menu [3]string
 	Menu[0] = "\n[1] Create Account" // Assign a value to the first element
-	Menu[1] = "\n[2]"                // Assign a value to the second element
-	Menu[2] = "\n[3]"                // Assign a value to the third element
+	Menu[1] = "\n[2] Edit Account"   // Assign a value to the second element
+	Menu[2] = "\n[3] View All"       // Assign a value to the third element
 	var CreateAccMenu [3]string
 	CreateAccMenu[0] = "\n[1] Passenger" // Assign a value to the first element
 	CreateAccMenu[1] = "\n[2] Driver"    // Assign a value to the second element
@@ -19,20 +19,18 @@ func UIMain() {
 	fmt.Println("Option: ")
 	fmt.Scanln(&MenuOption)
 	if MenuOption == 1 {
-		// s := "echo helloasdasd"
-		// args := strings.Split(s, "")
-		// cmd := exec.Command(args[0], args[1:]...)
-		// b, err := cmd.CombinedOutput()
-		// if err != nil {
-		// 	fmt.Printf("asdasd %v", err)
-		// }
-		// fmt.Printf("%s", b)
 		fmt.Println("-------------Create Account--------------", CreateAccMenu[0]+CreateAccMenu[1])
 		fmt.Println("Option: ")
 		fmt.Scanln(&CreateAccMenuOption)
 		if CreateAccMenuOption == 1 {
-			DriverMain.hello()
+			DriverMain.Hello()
+		} else if CreateAccMenuOption == 2 {
+			DriverMain.Hello()
+			jsonData := map[string]string{"ID": "t1212213o", "First Name": "Gerald", "Last Name": "Tan",
+				"Mobile No": "91234567", "Email Address": "Testing@test.com", "License No": "LKS123L", "Status": "Available"}
+			DriverMain.AddDriver("1", jsonData)
+			DriverMain.GetDriver("1")
+			DriverMain.AddDriverToDB()
 		}
 	}
-
 }
